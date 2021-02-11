327accdbd1d0e1eefb5a4fd223479e658bd89782
JushBJJ <36951064+JushBJJ@users.noreply.github.com>
2021-02-11 15:23:18 +0300

    Basic setup

---
+++ chart.py
@@ -0,0 +1,10 @@
+class Chart:
+    @staticmethod
+    def load_chart(symbol=None):
+        if symbol == None: raise BaseException("No symbol provided")
+        pass
+
+    @staticmethod
+    def update_chart(chart=None):
+        if chart == None: raise BaseException("No chart provided")
+        pass
